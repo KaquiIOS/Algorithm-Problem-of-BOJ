93d6f4715dd278f3bf7d52033fe1b681dee1f78e
Kaqui <kokoxg2@naver.com>
2018-05-03 23:56:04 +0900

    2018/05/03

--- Comparision Sorting.java
+++ Comparision Sorting.java
@@ -1,20 +1,41 @@
-
 package SortingAlgorithm;
 
-// 나중에 Generic 으로 변경하기
-
 import org.apache.commons.lang3.ArrayUtils;
-import java.util.Arrays;
 
 class Sort {
 
-    private static void swap(int idx1, int idx2, int input[]) {
+    private static void swap(int idx1, int idx2, String[] input) {
+        // 같은 인덱스면 return
+        if(idx1 == idx2) return;
+        // Swap
+        String tmp = input[idx1];
+        input[idx1] = input[idx2];
+        input[idx2] = tmp;
+    }
+
+    private static void swap(int idx1, int idx2, int[] input) {
         // 같은 인덱스면 return
         if(idx1 == idx2) return;
         // Swap
         int tmp = input[idx1];
         input[idx1] = input[idx2];
         input[idx2] = tmp;
+    }
+
+    private static void reverse(int[] input) {
+
+        int left = 0, right = input.length - 1;
+
+        while(left < right)
+            swap(left++, right--, input);
+    }
+
+    private static void reverse(String[] input) {
+
+        int left = 0, right = input.length - 1;
+
+        while(left < right)
+            swap(left++, right--, input);
     }
 
     /*
@@ -106,7 +127,8 @@ class Sort {
 
         // 오름차순으로 정렬되기때문에 asc == False 인 경우만 역순으로 배치한다.
         if(!asc)
-            ArrayUtils.reverse(input);
+            reverse(input);
+            //ArrayUtils.reverse(input);
     }
 
     /*
@@ -173,7 +195,8 @@ class Sort {
         quickSort(input, left, pivot);
 
         if(!asc)
-            ArrayUtils.reverse(input);
+            reverse(input);
+            //ArrayUtils.reverse(input);
     }
 
     /*
@@ -219,7 +242,7 @@ class Sort {
      * Parameter   : input : int[], asc : boolean
      * Description : 외부에서 접근하기 위해 만들어 놓은 heapSort 함수. asc = true/false (오름차순/내림차순)
      */
-    public final static void heapSort(int[] input, boolean asc) {
+    public final static void heapSort(String[] input, boolean asc) {
 
         // input 을 Heap 에 맞게 변경
         buildHeap(input);
@@ -228,7 +251,8 @@ class Sort {
 
         // 내림차순으로 정렬되기 때문에 asc == True 인 경우만 역순으로 배치
         if(asc)
-            ArrayUtils.reverse(input);
+            reverse(input);
+            //ArrayUtils.reverse(input);
     }
 
     /*
@@ -236,7 +260,7 @@ class Sort {
      * Parameter   : input : int[]
      * Description : heap의 형태를 만든 후, 루트노드를 반복적으로 POP 수행 -> 정렬된 배열 생성.
      */
-    private final static void heapSort(int[] input) {
+    private final static void heapSort(String[] input) {
 
         int numOfNodes = input.length - 1;
         // POP 한 노드를 맨 마지막 노드의 위치로 보내기 => 반복문 실행 후 내림차순으로 input이 정렬됨.
@@ -251,7 +275,7 @@ class Sort {
      * Parameter   : input : int[]
      * Description : 자식을 가질 수 있는 Level 부터 각각의 서브트리를 Heap 형태로 변경
      */
-    private final static void buildHeap(int[] input) {
+    private final static void buildHeap(String[] input) {
 
         // 배열이 0부터 시작하기에 - 1 을 한다.
         // 리프노드 Level 을 제외한 줄부터 Heap 에 맞게 변경
@@ -267,13 +291,21 @@ class Sort {
      * Parameter   : input : int[], parNode : int, length : int
      * Description : 부모노드(parNode)를 루트노드로 하는 서브트리를 Heap 에 맞게 변경
      */
-    private final static void heapify(int[] input, int parNode, int length) {
+    private final static void heapify(String[] input, int parNode, int length) {
 
         int smaller, leftChild = parNode * 2 + 1, rightChild = parNode * 2 + 2;
 
         // 오른쪽 자식이 있는 경우, 작은 쪽의 자식 선택
-        if(rightChild < length)
-            smaller = (input[leftChild] < input[rightChild]) ? leftChild : rightChild;
+        if(rightChild < length) {
+            // 문자열 길이가 더 짧은 노드를 선택
+            if(input[leftChild].length() < input[rightChild].length())
+                smaller = leftChild;
+            else if(input[leftChild].length() > input[rightChild].length())
+                smaller = rightChild;
+            // 두 문자열의 길이가 동일하다면 사전순서가 빠른 것을 선택(A.compareTo(B) -> A < B : -, A == B : 0, A < B : 0 +)
+            else
+                smaller = (input[leftChild].compareTo(input[rightChild]) < 0 ? leftChild : rightChild);
+        }
         // 왼쪽 자식만 있는 경우, 왼쪽 자식 선택
         else if(leftChild < length)
             smaller = leftChild;
@@ -281,12 +313,64 @@ class Sort {
         else
             return;
 
-        // 부모노드보다 자식 노드가 작은 경우 Swap 후 자식 노드를 루트로 하여 다시 Heap 에 맞게 변경
-        if(input[smaller] < input[parNode]) {
+        // 부모, 자식 문자열의 길이 비교 후 자식의 길이가 짧으면 부모-자식 변경. 부모를 루트로 다시 힙하게 변경
+        if(input[smaller].length() < input[parNode].length()) {
+            swap(smaller, parNode, input);
+            heapify(input, smaller, length);
+        }
+        // 부모, 자식의 길이가 같다면 사전순으로 변경
+        else if(input[smaller].length() == input[parNode].length() && input[smaller].compareTo(input[parNode]) < 0) {
             swap(smaller, parNode, input);
             heapify(input, smaller, length);
         }
     }
 
+
+    /*
+     * Method      : sortedIndexOf
+     * Parameter   : input : int[], idx : int
+     * Description : idx번째로 작은 값 반환
+     */
+    public final static int sortedIndexOf(int[] input, int idx) throws Exception{
+
+        if(input.length <= idx)
+            throw new Exception();
+
+        int val = select(input, 0, input.length - 1, idx);
+
+        return val;
+    }
+
+	/*
+     * Method      : select
+     * Parameter   : input : int[], left : int, right : int, idx : int
+     * Description : Pivot은 정렬된 위치이다. Pivot을 기준으로 IDX의 위치를 확인하고 Pivot 보다 작으면/크면 왼쪽/오른쪽으로 이동한다. 반복하여 수행.
+	 *               만약 Pivot 과 찾고자하는 idx가 같으면 함수 종료. 
+     */
+    private final static int select(int[] input, int left, int right, int idx) {
+
+        // 원소가 하나가 남았으면 해당 원소 리턴
+        if(left == right) return input[left];
+
+        // 찾고자하는 인덱스가 Pivot 을 기준으로 어디 위치해있는지 확인
+        int pivot = partition(input, left, right);
+
+        // 왼쪽에 위치한 경우
+        if(idx < pivot) return select(input, left, pivot - 1, idx);
+        // Pivot은 정렬된 위치를 가진다 => Pivot과 같으면 바로 pivot 자리 리턴
+        else if(pivot == idx) return input[pivot];
+        // 오른쪽에 위치한 경우
+        else return select(input, pivot + 1, right, idx);
+
+        /*
+        if(left <= right) {
+
+            int pivot = partition(input,left, right);
+
+            if(idx < pivot) return select(input, left, pivot - 1, idx);
+            else if(idx == pivot) return input[pivot];
+            else return select(input, pivot + 1, right, idx);
+        }*/
+    }
 }
 
